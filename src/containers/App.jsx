68cad61c9d3595e7b28aa00febdf0c1a8ce7e177
karvaroz <vargasriosk@gmail.com>
2022-04-29 11:29:23 -0500

    Estilo navbar

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <div className="container py-10 mx-auto flex flex-wrap place-content-center">
+        <img src="/assets/icons/banner.png" alt="" />
 
-export default App
+      </div>
+    </>
+  );
+};
+
+export default App;
