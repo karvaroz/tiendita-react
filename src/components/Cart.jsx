68834d5c07c9ab2220d089824169a61735928a87
karvaroz <vargasriosk@gmail.com>
2022-04-30 19:53:14 -0500

    Tiendita funcional

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { CartContext } from "../context/CartContext";
 import Swal from "sweetalert2";
 import ItemCart from "./ItemCart";
+import "../styles/Cart.css";
 
 const Cart = () => {
   const [productsLength, setProductsLength] = useState(0);
@@ -37,16 +38,41 @@ const Cart = () => {
       {productsLength === 0 ? (
         "Carrito vacio"
       ) : (
-        <div>
-          <h2>Productos en el carrito: {productsLength}</h2>
-          <div className="">
-            {cartItems.map((item) => (
-              <ItemCart key={item.id} item={item} />
-
-            ))}
+        <div className="flex justify-center my-6">
+          <div className="flex flex-col w-full p-8 text-gray-800 bg-white shadow-lg pin-r pin-y md:w-4/5 lg:w-4/5">
+            <div className="flex-1">
+              <table className="w-full text-sm lg:text-base" cellspacing="0">
+                <thead>
+                  <tr className="h-12 uppercase">
+                    <th className="hidden md:table-cell"></th>
+                    <th className="text-left">Product</th>
+                    <th className="lg:text-right text-left pl-5 lg:pl-0">
+                      <span className="lg:hidden" title="Quantity">
+                        Cant
+                      </span>
+                      <span className="hidden lg:inline">Cantidad</span>
+                    </th>
+                    <th className="hidden text-right md:table-cell">Precio</th>
+                    <th className="text-right">SubTotal</th>
+                    <th className="text-right">Acciones</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {cartItems.map((item) => (
+                    <ItemCart key={item.id} item={item} />
+                  ))}
+                </tbody>
+              </table>
+              <hr />
+              <h2 className="font-bold text-gray-800 mt-10">Total: ${total}</h2>
+              <button
+                onClick={handlePay}
+                className="flex w-90 px-10 py-3 mt-6 font-medium text-white uppercase bg-orange-600 rounded-full shadow item-center hover:bg-orange-700 focus:shadow-outline focus:outline-none"
+              >
+                Pagar
+              </button>
+            </div>
           </div>
-          <h2>Total: ${total}</h2>
-          <button onClick={handlePay}>Pagar</button>
         </div>
       )}
     </>
--- src/components/ItemCart.jsx
+++ src/components/ItemCart.jsx
@@ -5,17 +5,45 @@ const ItemCart = ({ item }) => {
   const { addItemToCart, deleteItemToCart } = useContext(CartContext);
 
   return (
-    <div className="cart-item" key={item.id}>
-      <img src={item.image} alt="product" />
-      <div>
-        <h4>{item.name}</h4>
-        <h4>${item.price}</h4>
-        <h4>Quantity: {item.quantity}</h4>
-        <h4>Subtotal:{item.price * item.quantity}</h4>
-        <button onClick={() => addItemToCart(item)}>+1</button>
-        <button onClick={() => deleteItemToCart(item)}>-1</button>
-      </div>
-    </div>
+    <tr className="linea_cart" id={item.id}>
+      <td className="hidden pb-4 md:table-cell">
+        <img src={item.image} alt={item.name} className="w-20" />
+      </td>
+      <td>
+        <p className="mb-2 md:ml-4">{item.name}</p>
+      </td>
+      <td className="justify-center md:justify-end md:flex mt-6">
+        <div className="w-20 h-10">
+          <div className="relative flex flex-row w-full h-8">
+            <p className="w-full font-semibold text-center text-gray-700 bg-gray-200 outline-none focus:outline-none hover:text-black focus:text-black">
+              {item.quantity}
+            </p>
+          </div>
+        </div>
+      </td>
+      <td className="hidden text-right md:table-cell">
+        <span className="text-sm lg:text-base font-medium">${item.price}</span>
+      </td>
+      <td className="text-right">
+        <span className="text-sm lg:text-base font-medium">
+          ${item.price * item.quantity}
+        </span>
+      </td>
+      <td className="text-end">
+        <button
+          className="bg-orange-600 hover:bg-orange-700 text-white font-bold py-1 px-1 border rounded ml-2"
+          onClick={() => addItemToCart(item)}
+        >
+          +1
+        </button>
+        <button
+          className="bg-orange-600 hover:bg-orange-700 text-white font-bold py-1 px-1 border rounded ml-2"
+          onClick={() => deleteItemToCart(item)}
+        >
+          -1
+        </button>
+      </td>
+    </tr>
   );
 };
 
